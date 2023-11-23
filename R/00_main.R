c698841074f3ae0777641a1cc4c4b456c5a757f3
Matthieu Lequien <matthieu.lequien@insee.fr>
2023-11-23 18:54:09 +0100

    load packages and file paths

--- R/00_main.R
+++ R/00_main.R
@@ -8,29 +8,33 @@ mainpath <- paste0(PATH_COFFRE_EMPREINTE_CARBONE, "/Ventilation empreinte carbon
 data_path <- paste0(PATH_COFFRE_EMPREINTE_CARBONE, "/output/data")
 graph_path <- paste0(PATH_COFFRE_EMPREINTE_CARBONE, "/output/graphiques/")
 
+# load packages
+pkglist <- c('haven', "dplyr", 'ggplot2', 'xtable', 'reshape2',  'tibble', 'tidyr', 'gridExtra', 'openxlsx', 'readxl', 'readr', 'readODS', 'knitr', 'kableExtra', 'hutils', "data.table", 'stringr')
+lapply(pkglist, library, character.only = TRUE)
+
 # category used to separate individuals
 ind_category <- "vingtieme" # "dixieme" "centieme" "millieme" "vingtieme"  "DNIVIE1"
 
 # 01 : load Budget de Famille data (confidential data)
-source("01_import_BdF.R")
+source("R/01_import_BdF.R")
 
 # 05 : load imported data
-source("05_load_imported_data.R")
+source("R/05_load_imported_data.R")
 
 # 06 : importe la nomenclature (pour avoir le nom des produits dans la base), et Agribalyse
-source("06_Import_Nomenclature_Agribalyse.R", encoding = "UTF-8")
+source("R/06_Import_Nomenclature_Agribalyse.R", encoding = "UTF-8")
 
 # 11 : creation des categories de menage
-source("11_households_categories.R")
+source("R/11_households_categories.R")
 
 # 12 : Nettoyage de la base de données carnets6 pour étudier l’effet prix
-source("12_selection_produits.R")
+source("R/12_selection_produits.R")
 
 # 14 : construire différents indicateurs de dispersion des prix payés selon les revenus des ménages
-source("14_effet_prix.R")
+source("R/14_effet_prix.R")
 
 # 15 : construire graphiques de biais lié à l’effet prix pour une sélection de produits
-source("15_effet_prix_selection_produits.R")
+source("R/15_effet_prix_selection_produits.R")
 
 # 17 : Figures with oils
-source("17_figures_oils.R")
+source("R/17_figures_oils.R")
